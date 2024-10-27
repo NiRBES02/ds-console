1324b4e01d338df6b50fd25484f98f3a5898ad10
NiRBES <98629663+NiRBES02@users.noreply.github.com>
2024-10-27 13:04:56 +0000

    Update to 1.2.0/no_comm

--- index.js
+++ index.js
@@ -1,67 +1,51 @@
-const Chalk = require('chalk'); // Импорт библиотеки для работы с цветами в консоли
-const Readline = require('readline'); // Импорт библиотеки для работы с вводом/выводом в консоли
+const Chalk = require('chalk');
+const Readline = require('readline');
 const rl = Readline.createInterface({
-  input: process.stdin, // Входной поток - стандартный ввод
-  output: process.stdout, // Выходной поток - стандартный вывод
-  terminal: true // Указываем, что это терминал
+  input: process.stdin,
+  output: process.stdout,
+  terminal: true
 });
-const Fs = require('fs'); // Импорт библиотеки для работы с файловой системой
+const Fs = require('fs');
 
 class Console {
   constructor() {
-    this.commands = new Map(); // Словарь для хранения команд
+    this.commands = new Map();
     this.colors = {
       success: '#00ff00',
-      // Цвет для успешных сообщений
       warning: '#ffff00',
-      // Цвет для предупреждений
       danger: '#ff0000',
-      // Цвет для ошибок
       info: '#00ffff',
-      // Цвет для информации
       primary: '#0077ff',
-      // Основной цвет
       secondary: '#666666',
-      // Вторичный цвет
-      magenta: '#ff66ee' // Цвет магента
+      magenta: '#ff66ee'
     }
     this.opt = {
       ident: 2.8,
-      // Отступ для идентификации
-      space: 10 // Пробелы для выравнивания
+      space: 10
     }
   }
 
-  // Метод для логирования сообщений в консоль
   async log(...args) {
-    const wrapAnsi = await import('wrap-ansi').then(m => m.default); // Импорт модуля для обрезки текста
-    const color = this.#getColor(args); // Получение цвета для сообщений
-    const exclude = this.#getColorName(args); // Получение имени цвета, если он указан
-    const filter = args.filter(arg => arg !== exclude); // Фильтрация аргументов
-    const time = new Date().toTimeString().split(' ')[0]; // Получение текущего времени
-    const timestamp = `${Chalk.underline.gray(`[${time}]`)}`; // Форматирование временной метки
+    const wrapAnsi = await import('wrap-ansi').then(m => m.default);
+    const color = this.#getColor(args);
+    const exclude = this.#getColorName(args);
+    const filter = args.filter(arg => arg !== exclude);
+    const time = new Date().toTimeString().split(' ')[0];
+    const timestamp = `${Chalk.underline.gray(`[${time}]`)}`;
     const arr = [];
-
-    // Обработка аргументов для логирования
     filter.forEach((arg) => {
-
       arr.push(this.dataTypeConvert(arg));
     });
-
-    const message = arr.join(' '); // Объединение сообщений
-    const coloredMessage = color ? color(message): message; // Применение цвета к сообщению
-    const consoleWidth = process.stdout.columns || 80; // Получение ширины консоли
-    const wrappedMessage = wrapAnsi(coloredMessage,
-      consoleWidth - (timestamp.length / this.opt.ident),
-      {
-        hard: true // Разбивать строки на жесткие границы
-      })
-    .split('\n')
-    .map((line, index) => index === 0 ? `${timestamp} ${line}`: `${' '.repeat(this.opt.space)} ${line}`)
-    .join('\n'); // Форматирование сообщения с временной меткой
-    console.log(wrappedMessage); // Вывод сообщения в консоль
+    const message = arr.join(' ');
+    const coloredMessage = color ? color(message) : message;
+    const consoleWidth = process.stdout.columns || 80;
+    const wrappedMessage = wrapAnsi(coloredMessage, 
+      consoleWidth - (timestamp.length / this.opt.ident), { hard: true })
+      .split('\n')
+      .map((line, index) => index === 0 ? `${timestamp} ${line}` : `${' '.repeat(this.opt.space)} ${line}`)
+      .join('\n');
+    console.log(wrappedMessage);
   }
-
 
   dataTypeConvert(value) {
     const anonymouse = 'anonymouse';
@@ -82,12 +66,10 @@ class Console {
       if (value instanceof RegExp) {
         return Chalk.magenta(value.toString());
       }
-      // Проверка на экземпляр класса
       if (value.constructor && value.constructor !== Object) {
         const className = value.constructor.name || anonymouse;
         return Chalk.cyan(`[class ${className}]`);
       }
-      // Сериализация обычного объекта
       return Chalk.yellow(JSON.stringify(value));
     }
     if (type === 'function') {
@@ -106,99 +88,86 @@ class Console {
     return value.toString();
   }
 
-
-  // Метод для получения цвета
   #getColor(args) {
     const array = args;
-    const matchedKeys = Object.keys(this.colors).filter(key => array.includes(key)); // Поиск совпадающих ключей
+    const matchedKeys = Object.keys(this.colors).filter(key => array.includes(key));
     if (matchedKeys.length > 0) {
-      const colorHex = this.colors[matchedKeys[0]]; // Получение цвета в шестнадцатеричном формате
-      return Chalk.hex(colorHex); // Возвращаем цвет
+      const colorHex = this.colors[matchedKeys[0]];
+      return Chalk.hex(colorHex);
     }
-    return null; // Если цвет не найден
+    return null;
   }
 
-  // Метод для получения имени цвета
   #getColorName(args) {
     const array = args;
-    const matchedKeys = Object.keys(this.colors).filter(key => array.includes(key)); // Поиск совпадающих ключей
+    const matchedKeys = Object.keys(this.colors).filter(key => array.includes(key));
     if (matchedKeys.length > 0) {
-      return matchedKeys.join(); // Возвращаем имена цветов
+      return matchedKeys.join();
     }
-    return null; // Если цвет не найден
+    return null;
   }
 
-  // Метод для проверки, является ли объектом
+  
   #isObject(obj) {
-    return typeof obj === 'object' && obj !== null; // Проверка на объект и не null
+    return typeof obj === 'object' && obj !== null;
   }
 
-  // Метод для парсинга аргументов из строки
   parseArgsFromString(input) {
     const parsed = {
       flags: {},
-      // Флаги команд
       values: {},
-      // Значения команд
-      unknown: [] // Неизвестные аргументы
+      unknown: []
     };
-    const args = input.split(' ') || input; // Разделение строки на аргументы
+    const args = input.split(' ') || input;
     for (let i = 0; i < args.length; i++) {
       const arg = args[i];
       if (arg.startsWith('--')) {
-        // Если аргумент начинается с --
         const [key,
-          value] = arg.slice(2).split('='); // Разделение ключа и значения
+          value] = arg.slice(2).split('=');
         if (value !== undefined) {
-          parsed.values[key] = value; // Добавление в значения
+          parsed.values[key] = value;
         } else {
-          parsed.flags[key] = true; // Добавление в флаги
+          parsed.flags[key] = true;
         }
       } else if (arg.startsWith('-')) {
-        // Если аргумент начинается с -
         const key = arg.slice(1);
-        parsed.flags[key] = true; // Добавление в флаги
+        parsed.flags[key] = true;
       } else {
-        parsed.unknown.push(arg); // Добавление в неизвестные аргументы
+        parsed.unknown.push(arg);
       }
     }
-    return parsed; // Возвращаем распарсенные аргументы
+    return parsed;
   }
 
-  // Метод для очистки консоли
   clear(bool = true) {
-    Readline.cursorTo(process.stdout, 0, 0); // Перемещение курсора в начало
-    Readline.clearScreenDown(process.stdout); // Очистка экрана
+    Readline.cursorTo(process.stdout, 0, 0);
+    Readline.clearScreenDown(process.stdout);
     if (bool) {
-      this.log('Консоль очищена'); // Логирование сообщения об очистке
+      this.log('Консоль очищена');
     }
   }
 
-  // Метод для регистрации команд
   cmd(...args) {
-    let name; // Имя команды
-    let description; // Описание команды
-    let value; // Функция команды
-    let unknown = []; // Неизвестные аргументы
+    let name;
+    let description;
+    let value;
+    let unknown = [];
     for (let i = 0; i < args.length; i++) {
       const arg = args[i];
-
       if (typeof arg === 'string') {
         if (arg.split(' ').length === 1) {
-          name = arg; // Запоминаем имя команды
         } else {
-          description = arg; // Запоминаем описание
+          description = arg;
         }
       } else if (typeof arg === 'object') {
         if (arg.description) {
-          description = arg.description; // Запоминаем описание из объекта
+          description = arg.description;
         }
       } else if (typeof arg === 'function') {
-        value = arg; // Запоминаем функцию
+        value = arg;
       } else {
-        unknown.push(arg); // Добавляем неизвестные аргументы
+        unknown.push(arg);
       }
-      // Если имя и функция определены, добавляем команду в словарь
       if (name && value) {
         this.commands.set(name, {
           name,
@@ -206,43 +175,41 @@ class Console {
           value,
           unknown
         });
-        name = undefined; // Сбрасываем имя
-        description = undefined; // Сбрасываем описание
-        value = undefined; // Сбрасываем функцию
-        unknown = []; // Сбрасываем неизвестные аргументы
+        name = undefined;
+        description = undefined;
+        value = undefined;
+        unknown = [];
       }
     }
   }
 
-  // Метод для замены плейсхолдеров в строке
   placeholder(str, obj) {
     const replace = str.replace(/{(.*?)}/g, (match, key) => {
-      const keys = key.split('.'); // Разделение ключей
+      const keys = key.split('.');
       let value = obj;
       for (const k of keys) {
-        value = value[k]; // Получение значения по ключу
+        value = value[k];
         if (value === undefined) {
-          return match; // Если значение не найдено, возвращаем оригинал
+          return match;
         }
       }
-      return value || match; // Возвращаем значение или оригинал
+      return value || match;
     });
-    return replace.toString(); // Возвращаем строку с заменами
+    return replace.toString();
   }
 
-  // Метод для отображения справки по командам
   help() {
-    const obj = Object.fromEntries(this.commands); // Преобразование команд в объект
-    const dir = __dirname; // Получение директории текущего файла
+    const obj = Object.fromEntries(this.commands);
+    const dir = __dirname;
     const file = Fs.readFileSync(`${dir}/help.txt`,
-      'utf8'); // Чтение файла с помощью
+      'utf8');
     Object.entries(obj).forEach(([key, value]) => {
-      const description = (value.description === undefined) ? Chalk.red('Отсутствует'): value.description; // Проверка наличия описания
+      const description = (value.description === undefined) ? Chalk.red('Отсутствует') : value.description;
       const unknown = () => {
         if (Array.isArray(value.unknown) && value.unknown.length > 0) {
-          return `debug: ${Chalk.red(value.unknown.join(', '))}`; // Если есть неизвестные аргументы
+          return `debug: ${Chalk.red(value.unknown.join(', '))}`;
         } else {
-          return `debug: ${Chalk.green('ok')}`; // Если нет
+          return `debug: ${Chalk.green('ok')}`;
         }
       };
       this.log(this.placeholder(file, {
@@ -259,43 +226,41 @@ class Console {
     });
   }
 
-  // Метод для обработки ввода пользователя
   handler() {
     rl.question('',
       (input) => {
-        const cmd = input.split(' ')[0]; // Получение команды
-        const args = this.parseArgsFromString(input); // Парсинг аргументов
+        const cmd = input.split(' ')[0];
+        const args = this.parseArgsFromString(input);
         if (this.commands.has(cmd)) {
-          this.commands.get(cmd).value(args); // Выполнение команды
+          this.commands.get(cmd).value(args);
         } else {
-          this.log(`Неизвестная команда: ${cmd}`, 'danger'); // Сообщение об ошибке
+          this.log(`Неизвестная команда: ${cmd}`, 'danger');
         }
-        this.handler(); // Запрос следующего ввода
+        this.handler();
       });
   }
 }
 
-const new_console = new Console(); // Создание экземпляра консоли
+const new_console = new Console();
 
-new_console.handler(); // Запуск обработчика ввода
-new_console.clear(false); // Очистка консоли при запуске
+new_console.handler();
+new_console.clear(false);
 
-// Регистрация команд
 new_console.cmd('clear', {
   description: 'Очистить консоль'
 }, () => new_console.clear());
 new_console.cmd('stop', {
   description: 'Остановить приложение'
 }, (cmd) => {
-  new_console.log('Закрытие приложения...'); // Сообщение о закрытии
-  setInterval(() => process.exit(0), 100); // Завершение процесса
+  new_console.log('Закрытие приложения...');
+  setInterval(() => process.exit(0), 100);
 });
 new_console.cmd('help', {
   description: 'Получить помощь по командам'
 }, () => {
-  new_console.log('Помощь по командам:', 'magenta'); // Сообщение о помощи
-  new_console.help(); // Вызов справки
+  new_console.log('Помощь по командам:', 'magenta');
+  new_console.help();
 });
 
-module.exports = new_console; // Экспорт экземпляра консоли
-module.exports.chalk = Chalk; // Экспорт библиотеки Chalk
+module.exports = new_console;
+module.exports.chalk = Chalk;
